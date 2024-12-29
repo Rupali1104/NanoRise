5fdc66ae8aa7db22fe42f9ed366ab5e458d61402
paiabhishekpai <abhishekupai1124@gmail.com>
2024-12-29 09:53:05 +0530

    UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/NavBar";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import HowItWorks from "./pages/HowItWorks";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Dashboard } from "./pages/Dashboard";
+import LoanAnalysis from './pages/LoanAnalysis';
+import { Footer } from "./components/layout/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Routes without Navbar and Footer */}
+        <Route
+          path="/login"
+          element={
+            <div id="root">
+              <Login />
+            </div>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <div id="root">
+              <Register />
+            </div>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <div id="root">
+              <Dashboard />
+            </div>
+          }
+        />
+
+<Route path="/LoanAnalysis" element={<div id="root">
+              <LoanAnalysis />
+            </div>} />
+
+        {/* Routes with Navbar and Footer */}
+        <Route
+          path="*"
+          element={
+            <>
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/howitworks" element={<HowItWorks />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
